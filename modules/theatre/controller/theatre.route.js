6a94633e0cd51967a03bdc414d21d3e3af0ba9bb
Parwat Kunwar <parwatkunwar08@gmail.com>
2018-08-17 22:07:03 +0545

    Added authorization in route

--- modules/theatre/controller/theatre.route.js
+++ modules/theatre/controller/theatre.route.js
@@ -8,13 +8,14 @@ import {
   deleteTheatreByID
 } from "./theatre.controller";
 import authentication from "../../../middlewares/authentication";
+import authorization from "../../../middlewares/authorization";
 
 const router = Router();
 
 router.get("/", getTheatres);
 router.get("/:theatreID", getTheatreByID);
-router.post("/", authentication, postTheatre);
-router.put("/:theatreID", authentication, updateTheatreByID);
-router.delete("/:theatreID", authentication, deleteTheatreByID);
+router.post("/", authentication, authorization, postTheatre);
+router.put("/:theatreID", authentication, authorization, updateTheatreByID);
+router.delete("/:theatreID", authentication, authorization, deleteTheatreByID);
 
 export default router;
